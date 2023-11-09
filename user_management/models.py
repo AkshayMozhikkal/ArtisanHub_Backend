12a67d76591c64e596664ffd26169e6d653703be
AkshayMozhikkal <akshay.for.career@gmail.com>
2023-11-09 13:52:54 +0530

    changed config to os.getenv

--- user_management/models.py
+++ user_management/models.py
@@ -1,7 +1,33 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+
+        if extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True.")
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 
@@ -19,12 +45,11 @@ class Uuser(AbstractUser):
     about = models.TextField(blank=True, null=True)
     field = models.CharField(max_length=60, null=True, blank=True)
     
-    
-    # user_connections = models.ManyToManyField(Connection, related_name='user_connections')
-    # user_works = models.ManyToManyField(Work, related_name='user_works')
      
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+    
+    objects=CustomUserManager()
     
     
 class Address(models.Model):
